d98d97ac9b123cdf7be064390dcb08e9d5227065
Adam England <adam.n.england@gmail.com>
2014-07-29 16:07:34 -0500

    Adding default route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def show()
+  def index
+    redirect_to: 'http://www.knoda.com'
+  end
+
+  def show
     su = ShortUrl.where(:slug => params[:slug]).first
     if su
       redirect_to su.long_url
